806be1a3d98ce9f1cf4cba32c1d27d8768b910df
Greg Bailey <gbailey@lxpro.com>
2018-05-03 14:09:39 -0700

    Initial revision

---
+++ make_box.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+# Amazon Linux 2 vagrant box construction, using an Amazon supplied VDI disk
+# image as a base.  This approach avoids actually booting the Amazon supplied
+# VDI disk image by mounting it and applying vagrant related changes to it, and
+# then calling vagrant to package the resulting image as a box file.  Available
+# updates are applied, except the kernel, since we're not actually running on
+# Amazon Linux 2 (this script was tested on a CentOS 7 installation).
+
+# Greg Bailey <gbailey@lxpro.com>
+# May 3, 2018
+
+set -x
+
+# https://cdn.amazonlinux.com/os-images/2017.12.0.20180330/virtualbox/amzn2-virtualbox-2017.12.0.20180330-x86_64.xfs.gpt.vdi
+AMZN2_SRC="/iso/amzn2-virtualbox-2017.12.0.20180330-x86_64.xfs.gpt.vdi"
+AMZN2_RAW="$(mktemp -d -t amzn2_raw_XXXXXXXX)"
+AMZN2_MNT="$(mktemp -d -t amzn2_mnt_XXXXXXXX)"
+AMZN2_VDI="$(mktemp -d -t amzn2_vdi_XXXXXXXX)"
+
+qemu-img convert ${AMZN2_SRC} ${AMZN2_RAW}/amzn2.raw
+
+# Mount the raw image and prepare to chroot into it
+
+mount -o loop,offset=2097152 ${AMZN2_RAW}/amzn2.raw ${AMZN2_MNT}
+
+cp -a setup.sh ${AMZN2_MNT}/.
+
+mount -o bind /dev ${AMZN2_MNT}/dev
+mount -o bind /proc ${AMZN2_MNT}/proc
+mount -o bind /sys ${AMZN2_MNT}/sys
+
+chroot ${AMZN2_MNT} /setup.sh
+
+umount ${AMZN2_MNT}/dev
+umount ${AMZN2_MNT}/proc
+umount ${AMZN2_MNT}/sys
+
+umount ${AMZN2_MNT}
+
+# Manually construct a virtualbox VM and add the new image to it
+
+vboxmanage convertfromraw ${AMZN2_RAW}/amzn2.raw ${AMZN2_VDI}/amzn2.vdi --format VDI
+
+vboxmanage createvm --name amzn2 --ostype Linux26_64 --register
+vboxmanage modifyvm amzn2 --memory 1024 --vram 16 --audio none
+
+vboxmanage storagectl amzn2 --name IDE --add ide
+vboxmanage storagectl amzn2 --name SATA --add sata --portcount 1
+
+vboxmanage storageattach amzn2 --storagectl IDE --port 1 --device 0 --type dvddrive --medium emptydrive
+vboxmanage storageattach amzn2 --storagectl SATA --port 0 --device 0 --type hdd --medium ${AMZN2_VDI}/amzn2.vdi
+
+# Package resulting VM
+
+vagrant package --base amzn2 --output amzn2.box
+
+# Cleanup
+
+vboxmanage unregistervm amzn2 --delete
+
+rm -rf ${AMZN2_RAW}
+rm -rf ${AMZN2_MNT}
+rm -rf ${AMZN2_VDI}
---
+++ setup.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+# Amazon Linux 2 vagrant box construction, using an Amazon supplied VDI disk
+# image as a base.  This script runs inside of a mounted Amazon Linux 2 VDI
+# disk image, and sets up the vagrant related changes.
+
+# Greg Bailey <gbailey@lxpro.com>
+# May 3, 2018
+
+set -x
+
+VIRTUALBOX_VERSION="5.2.10"
+
+# The image doesn't have any resolvers specified
+
+echo "nameserver 8.8.8.8" > /etc/resolv.conf
+
+# Set up vagrant user
+
+useradd vagrant
+
+mkdir -p /home/vagrant/.ssh
+wget -nv https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub -O /home/vagrant/.ssh/authorized_keys
+chmod 600 /home/vagrant/.ssh/authorized_keys
+chmod 700 /home/vagrant/.ssh
+chown -R vagrant.vagrant /home/vagrant
+
+echo 'vagrant ALL=(ALL) NOPASSWD: ALL' > /etc/sudoers.d/vagrant
+chmod 440 /etc/sudoers.d/vagrant
+
+# Apply any available Amazon Linux 2 updates
+
+yum -y upgrade --exclude='kernel*'
+
+# Install the VirtualBox guest additions
+
+KERNEL_VERSION=$(ls /lib/modules)
+
+yum -y install gcc elfutils-libelf-devel kernel-devel-${KERNEL_VERSION}
+
+wget -nv https://download.virtualbox.org/virtualbox/${VIRTUALBOX_VERSION}/VBoxGuestAdditions_${VIRTUALBOX_VERSION}.iso -O /root/VBoxGuestAdditions.iso
+mount -o ro,loop /root/VBoxGuestAdditions.iso /mnt
+sh /mnt/VBoxLinuxAdditions.run
+umount /mnt
+rm -f /root/VBoxGuestAdditions.iso
+
+# The above will generate error messages because the running kernel isn't an
+# Amazon kernel; the below commands explicitly run VirtualBox guest additions
+# setup for the Amazon provided kernel so that it won't have to be done when
+# the Amazon Linux 2 virtual machine is booted.
+
+/etc/kernel/postinst.d/vboxadd ${KERNEL_VERSION}
+/sbin/depmod ${KERNEL_VERSION}
+
+# Clean up temporary files
+
+yum clean all
+rm -rf /var/cache/yum/*
+
+rm -f /etc/resolv.conf
+
+rm -f /setup.sh
+
+# Free up space used by removed files
+
+for i in `seq 10`; do sync; cat /dev/zero > /zero$i; sleep 1; done
+rm -f /zero*
