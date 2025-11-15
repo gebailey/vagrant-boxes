78678444d7bbe9b233f22b99a4463a746aacf099
Greg Bailey <gbailey@lxpro.com>
2025-11-14 18:18:59 -0700

    Update to al2023-2023.9.20251110.1

--- al2023/make_box.sh
+++ al2023/make_box.sh
@@ -13,7 +13,7 @@ set -eux
 # Latest version can be retrieved using:
 # curl -I https://cdn.amazonlinux.com/al2023/os-images/latest/
 
-AL2023_VERSION="2023.8.20250908.0"
+AL2023_VERSION="2023.9.20251110.1"
 
 AL2023_OVA="al2023-vmware_esx-${AL2023_VERSION}-kernel-6.1-x86_64.xfs.gpt.ova"
 AL2023_SRC="al2023-vmware_esx-${AL2023_VERSION}-kernel-6.1-x86_64.xfs.gpt-disk1.vmdk"
